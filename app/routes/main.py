836f2ec6a0f4a2a307246e508fd3c519f1df6dc6
Ryan Jasinski <jasinski.17@gmail.com>
2025-06-19 08:34:45 -0700

    update main route message

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+import datetime
 
 bp = Blueprint("main", __name__)
 
@@ -8,7 +9,12 @@ route_prefix = ""
 @bp.route("/", methods=["GET"])
 def main():
 
-    return jsonify({"status": "ok"})
+    return jsonify(
+        {
+            "status": "ok",
+            "current_time": datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        }
+    )
 
 
 def get_blueprint():
