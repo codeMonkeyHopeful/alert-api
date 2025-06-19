47efe8b772144d2add8dc71c879d1dd021616683
Ryan Jasinski <jasinski.17@gmail.com>
2025-06-19 06:24:29 -0700

    rewrite auth checks

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,10 +5,27 @@ from flask_jwt_extended import (
     jwt_required,
     create_refresh_token,
 )
+import bcrypt
 
 bp = Blueprint("auth", __name__)
 
 route_prefix = "/auth"
+
+
+def hash_password(plain_text_password):
+    return bcrypt.hashpw(plain_text_password.encode(), bcrypt.gensalt())
+
+
+def check_password(plain_text_password, hashed_password):
+    if isinstance(hashed_password, str):
+        hashed_password = hashed_password.encode()
+    return bcrypt.checkpw(plain_text_password.encode(), hashed_password)
+
+
+# Fake user for demonstration purposes
+db_password = hash_password("test")
+# This is how it would be stored in a database
+fake_user = {"username": "test", "password": db_password, "id": 1}
 
 
 @bp.route("/login", methods=["POST"])
@@ -16,19 +33,28 @@ def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     # TODO: Implement real authentication logic
-    if username != "test" or password != "test":
+
+    # Call to the DB here to get the user information
+    # need to test if user exists and ony then proceed.
+
+    # Simulate DB lookup
+    if username != fake_user["username"]:
         return jsonify({"msg": "Unauthorized"}), 401
 
-    access_token = create_access_token(identity=username)
-    refresh_token = create_refresh_token(identity=username)
+    # Check password using bcrypt
+    if not check_password(password, fake_user["password"]):
+        return jsonify({"msg": "Unauthorized"}), 401
+
+    access_token = create_access_token(identity=fake_user["id"])
+    refresh_token = create_refresh_token(identity=fake_user["id"])
     return jsonify(access_token=access_token, refresh_token=refresh_token)
 
 
 @bp.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
 def refresh():
-    current_user = get_jwt_identity()
-    new_access_token = create_access_token(identity=current_user)
+    current_user_id = get_jwt_identity()
+    new_access_token = create_access_token(identity=current_user_id)
     return jsonify(access_token=new_access_token)
 
 
